086d7d95b1cf1059d6bb8e9ec5feca1f3a94bbdc
Skygaming <ahmadpassau@gmail.com>
2024-08-19 08:21:21 +0000

    feat: initiated the project

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x512A93DE2f63A21a43723aD6420c633C5438c6E7'
+export const ProxyAdmin = '0xF57d8f0FFe9EfF1f37E3854bB8C277f736901Da1'
+export const SWTRImplementation = '0xec057D8b5876caBC3696BD7259a5C0295167989E'
     
